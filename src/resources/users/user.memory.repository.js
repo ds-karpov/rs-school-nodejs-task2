01190564115ca9b0b69f32980a7cb974988013cd
Denis Karpov <karpov.syned@gmail.com>
2020-10-12 16:28:04 +0300

    feat: add logic for \users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,41 @@
+const memoryDB = require('../../common/db');
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return memoryDB.users;
 };
 
-module.exports = { getAll };
+const getOne = async id => {
+  return memoryDB.users.find(el => el.id === id);
+};
+
+const create = async user => {
+  memoryDB.users.push(user);
+};
+
+const update = async user => {
+  const { id } = user;
+  const index = memoryDB.users.findIndex(el => el.id === id);
+  if (index > -1) {
+    memoryDB.users = [
+      ...memoryDB.users.slice(0, index),
+      user,
+      ...memoryDB.users.slice(index + 1)
+    ];
+    return true;
+  }
+  return false;
+};
+
+const remove = async id => {
+  const index = memoryDB.users.findIndex(el => el.id === id);
+  if (index > -1) {
+    memoryDB.users = memoryDB.users.filter(user => user.id !== id);
+    memoryDB.tasks = memoryDB.tasks.map(task =>
+      task.userId === id ? { ...task, userId: null } : task
+    );
+    return true;
+  }
+  return false;
+};
+
+module.exports = { getAll, getOne, create, update, remove };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,10 +2,47 @@ const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router
+  .route('/')
+  .get(async (req, res) => {
+    const users = await usersService.getAll();
+    res.status(200).send(users.map(User.toResponse));
+  })
+  .post(async (req, res) => {
+    const user = new User(req.body);
+    await usersService.create(user);
+    res.status(200).send(User.toResponse(user));
+  });
+
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    const user = await usersService.getOne(req.params.id);
+    if (user) {
+      res.status(200).send(User.toResponse(user));
+    } else {
+      res.sendStatus(404);
+    }
+  })
+  .put(async (req, res) => {
+    const user = {
+      ...req.body,
+      id: req.params.id
+    };
+    const isUpdated = await usersService.update(user);
+    if (isUpdated) {
+      res.status(200).send(User.toResponse(user));
+    } else {
+      res.sendStatus(400);
+    }
+  })
+  .delete(async (req, res) => {
+    const user = await usersService.remove(req.params.id);
+    if (user) {
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  });
 
 module.exports = router;
